897d11bb2d53fe4337036238345a424783fa903e
Piotr Karaś <piotr.karas@spoton.com>
2024-12-17 15:13:46 +0100

    feat: added data_generation module

---
+++ src/data_generation/datasets.py
@@ -0,0 +1,55 @@
+from itertools import product
+
+
+def even_number_of_as() -> tuple[list[str], list[str]]:
+    positive_examples = ["", "b", "aa", "bb", "aab", "baa", "aba", "abaaa", "baaaa"]
+    negative_examples = ["a", "ab", "ba", "aaa", "aaba", "abb", "ababab"]
+    return positive_examples, negative_examples
+
+
+def even_number_of_as_or_bs() -> tuple[list[str], list[str]]:
+    positive_examples = ["", "a", "b", "aa", "bb", "abb", "aab", "baa", "aba", "abaaa", "baaaa"]
+    negative_examples = ["ab", "ba", "aaab", "aaba", "abaa", "baaa", "abbb", "bbba", "ababab"]
+    return positive_examples, negative_examples
+
+
+def at_least_one_a() -> tuple[list[str], list[str]]:
+    positive_examples = ["a", "aa", "ab", "ba", "aba", "aab", "baa", "abaaa", "baaaa", "ac"]
+    negative_examples = ["", "b", "bb", "c", "bc", "bbb", "bbb", "bbbc"]
+    return positive_examples, negative_examples
+
+
+def one_is_third_from_end() -> tuple[list[str], list[str]]:
+    positive_examples = [
+        "100",
+        "101",
+        "110",
+        "111",
+        "000101010101110000100100",
+    ]
+    negative_examples = [
+        "",
+        "0",
+        "1",
+        "10",
+        "11",
+        "01",
+        "00",
+        "000",
+        "001",
+        "010",
+        "011",
+        "0001010101011111100000000000",
+    ]
+    bin6 = ["".join(bits) for bits in product("01", repeat=6)]
+    for bin_str in bin6:
+        negative_examples.append(f"{bin_str}000")
+        negative_examples.append(f"{bin_str}001")
+        negative_examples.append(f"{bin_str}010")
+        negative_examples.append(f"{bin_str}011")
+        positive_examples.append(f"{bin_str}100")
+        positive_examples.append(f"{bin_str}101")
+        positive_examples.append(f"{bin_str}110")
+        positive_examples.append(f"{bin_str}111")
+
+    return positive_examples, negative_examples
---
+++ src/lstar_driver.py
@@ -0,0 +1,47 @@
+from aalpy import Dfa, RandomWalkEqOracle, run_Lstar
+from aalpy.SULs import DfaSUL
+
+
+def run_lstar() -> None:
+    # state_setup = {
+    #     'q0': (True, {'a': 'q1', 'b': 'q2'}),
+    #     'q1': (False, {'a': 'q0', 'b': 'q3'}),
+    #     'q2': (False, {'a': 'q3', 'b': 'q0'}),
+    #     'q3': (False, {'a': 'q2', 'b': 'q1'})
+    # }
+    state_setup = {
+        'q0': (True, {'a': 'q1', 'b': 'q2'}),
+        'q1': (True, {'a': 'q0', 'b': 'q3'}),
+        'q2': (True, {'a': 'q3', 'b': 'q0'}),
+        'q3': (False, {'a': 'q2', 'b': 'q1'})
+    }
+    dfa = Dfa.from_state_setup(state_setup=state_setup)
+
+    # Get its input alphabet
+    alphabet = dfa.get_input_alphabet()
+
+    # Create a SUL instance weapping the Anguin's automaton
+    sul = DfaSUL(dfa)
+
+    # create a random walk equivelance oracle that will perform up to 500 steps every learning round
+    eq_oracle = RandomWalkEqOracle(alphabet, sul, 500, reset_after_cex=True)
+
+    # start the L* and print the whole process in detail
+    learned_dfa = run_Lstar(
+        alphabet,
+        sul,
+        eq_oracle,
+        automaton_type="dfa",
+        cache_and_non_det_check=True,
+        cex_processing=None,
+        print_level=3,
+    )
+
+    print(learned_dfa)
+    learned_dfa.visualize(path="data/learned_dfa", file_type="png")
+    learned_dfa.save(file_path="data/learned_dfa")
+    
+    
+if __name__ == "__main__":
+    run_lstar()
+    
